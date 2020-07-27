54691f7e55d46765df3bf1691185928e5f00c855
Clemens Brunner <clemens.brunner@gmail.com>
2020-07-27 14:16:39 +0200

    Refactor Python benchmarks

--- benchmark_python.py
+++ benchmark_python.py
@@ -3,85 +3,110 @@ import random
 import numpy as np
 
 
+functions = []
+
+
+def register(f):
+    functions.append(f)
+    return f
+
+
+@register
 def test_builtin_sum():
-    """Builtin sum of 10**7 numbers."""
+    """Benchmark builtin sum of 10**7 numbers."""
     random.seed(1)
-    x = [random.uniform(0, 1) for _ in range(10**7)]
+    a = [random.uniform(0, 1) for _ in range(10**7)]  # generator is faster
     n = 100
-    r = timeit(lambda: sum(x), number=n) / n
+    r = timeit(lambda: sum(a), number=n) / n
     return f"python,builtin,sum,{r}"
 
 
-# custom sum
-def sum1(x):
+def _sum(numbers):
     """Compute sum of elements in x."""
     s = 0
-    for e in x:
-        s += e
+    for n in numbers:
+        s += n
     return s
 
 
+@register
 def test_custom_sum():
-    """Custom sum of 10**7 numbers."""
+    """Benchmark custom sum of 10**7 numbers."""
     random.seed(1)
-    x = [random.uniform(0, 1) for _ in range(10**7)]
+    a = [random.uniform(0, 1) for _ in range(10**7)]
     n = 10
-    r = timeit(lambda: sum1(x), number=n) / n
+    r = timeit(lambda: _sum(a), number=n) / n
     return f"python,custom,sum,{r}"
 
 
+@register
 def test_numpy_sum():
-    """NumPy sum."""
-    rng = np.random.default_rng(1)
-    x = rng.uniform(size=(10**7))
+    """Benchmark NumPy sum."""
+    a = np.random.default_rng(1).uniform(size=(10**7))
     n = 100
-    r = timeit(lambda: np.sum(x), number=n) / n
+    r = timeit(lambda: np.sum(a), number=n) / n
     return f"python,numpy,sum,{r}"
 
 
-# matrix multiplication
-rng = np.random.default_rng(1)
-x = rng.uniform(size=(10000, 2000))
-y = x.T
-n = 5
-r = timeit(lambda: x @ y, number=n) / n
-print(f"python,numpy,matrix_multiplication,{r}")
+@register
+def test_matrix_multiplication():
+    """Benchmark matrix multiplication."""
+    a = np.random.default_rng(1).uniform(size=(10000, 2000))
+    b = a.T
+    n = 5
+    r = timeit(lambda: a @ b, number=n) / n
+    return f"python,numpy,matrix_multiplication,{r}"
 
-# Cholesky decomposition
-rng = np.random.default_rng(1)
-x = rng.uniform(size=(3000, 3000))
-y = x.T
-z = x @ y
-n = 10
-r = timeit(lambda: np.linalg.cholesky(z), number=n) / n
-print(f"python,numpy,cholesky,{r}")
 
-# SVD
-rng = np.random.default_rng(1)
-x = rng.uniform(size=(5000, 2000))
-n = 5
-r = timeit(lambda: np.linalg.svd(x), number=n) / n
-print(f"python,numpy,svd,{r}")
+@register
+def test_cholesky():
+    """Benchmark Cholesky decomposition."""
+    a = np.random.default_rng(1).uniform(size=(3000, 3000))
+    b = a.T
+    c = a @ b
+    n = 10
+    r = timeit(lambda: np.linalg.cholesky(c), number=n) / n
+    return f"python,numpy,cholesky,{r}"
 
-# determinant
-rng = np.random.default_rng(1)
-x = rng.uniform(size=(3000, 3000))
-n = 10
-r = timeit(lambda: np.linalg.det(x), number=n) / n
-print(f"python,numpy,det,{r}")
 
-# linear equation system
-rng = np.random.default_rng(1)
-x = rng.uniform(size=(2000, 2000))
-y = np.arange(2000)
-n = 100
-r = timeit(lambda: np.linalg.solve(x, y), number=n) / n
-print(f"python,numpy,solve,{r}")
+@register
+def test_svd():
+    """Benchmark SVD."""
+    a = np.random.default_rng(1).uniform(size=(5000, 2000))
+    n = 3
+    r = timeit(lambda: np.linalg.svd(a), number=n) / n
+    return f"python,numpy,svd,{r}"
 
-# least squares
-rng = np.random.default_rng(1)
-x = rng.uniform(size=(2000, 1500))
-y = np.arange(2000)
-n = 10
-r = timeit(lambda: np.linalg.lstsq(x, y, rcond=None), number=n) / n
-print(f"python,numpy,lstsq,{r}")
+
+@register
+def test_det():
+    """Benchmark determinant."""
+    a = np.random.default_rng(1).uniform(size=(500, 500))
+    n = 100
+    r = timeit(lambda: np.linalg.det(a), number=n) / n
+    return f"python,numpy,det,{r}"
+
+
+@register
+def test_solve():
+    """Benchmark solving a linear equation system."""
+    a = np.random.default_rng(1).uniform(size=(2000, 2000))
+    b = np.arange(2000)
+    n = 50
+    r = timeit(lambda: np.linalg.solve(a, b), number=n) / n
+    return f"python,numpy,solve,{r}"
+
+
+@register
+def test_lstsq():
+    """Benchmark least squares solution."""
+    a = np.random.default_rng(1).uniform(size=(2000, 1500))
+    b = np.arange(2000)
+    n = 5
+    r = timeit(lambda: np.linalg.lstsq(a, b, rcond=None), number=n) / n
+    return f"python,numpy,lstsq,{r}"
+
+
+if __name__ == "__main__":
+    for f in functions:
+        print(f())
