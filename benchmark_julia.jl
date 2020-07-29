1dc3b8317cb1b673ae2719bf563affdd4007e1b3
Clemens Brunner <clemens.brunner@gmail.com>
2020-07-29 18:21:05 +0200

    Finalize Julia benchmarks

--- benchmark_julia.jl
+++ benchmark_julia.jl
@@ -34,3 +34,25 @@ b = copy(a)'
 c = a * b
 t = @benchmark cholesky($c)
 println("julia,cholesky,$(minimum(t.times) * 1e-9)")
+
+# SVD
+a = rand(Float64, (5000, 2000))
+t = @benchmark svd($a)
+println("julia,svd,$(minimum(t.times) * 1e-9)")
+
+# determinant
+a = rand(Float64, (500, 500))
+t = @benchmark det($a)
+println("julia,det,$(minimum(t.times) * 1e-9)")
+
+# solve a linear equation system
+a = rand(Float64, (2000, 2000))
+b = 1:2000
+t = @benchmark $a \ $b
+println("julia,solve,$(minimum(t.times) * 1e-9)")
+
+# least squares solution
+a = rand(Float64, (2000, 1500))
+b = 1:2000
+t = @benchmark $a \ $b
+println("julia,lstsqr,$(minimum(t.times) * 1e-9)")
