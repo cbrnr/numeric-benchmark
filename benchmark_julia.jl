e35e44a9c1ce3c00bd5c1040b6573d5e6e6c1981
Clemens Brunner <clemens.brunner@gmail.com>
2020-07-30 11:30:48 +0200

    Include BLAS vendor

--- benchmark_julia.jl
+++ benchmark_julia.jl
@@ -3,6 +3,15 @@ using LinearAlgebra
 using BenchmarkTools
 
 
+if occursin("openblas", string(LinearAlgebra.BLAS.vendor()))
+    BLAS = "openblas"
+elseif occursin("mkl", string(LinearAlgebra.BLAS.vendor()))
+    BLAS = "mkl"
+else
+    BLAS = "unknown"
+end
+
+
 function _sum(numbers)
     s = 0.0
     for n in numbers
@@ -16,43 +25,43 @@ end
 Random.seed!(1)
 a = rand(10^7)
 t = @benchmark sum($a)
-println("julia,builtin_sum,$(minimum(t.times) * 1e-9)")
+println("julia,$BLAS,builtin_sum,$(minimum(t.times) * 1e-9)")
 
 # custom sum of 10**7 numbers
 t = @benchmark _sum($a)
-println("julia,custom_sum,$(minimum(t.times) * 1e-9)")
+println("julia,$BLAS,custom_sum,$(minimum(t.times) * 1e-9)")
 
 # matrix multiplication
 a = rand(Float64, (10000, 2000))
 b = copy(a)'
 t = @benchmark $a * $b
-println("julia,matrix_multiplication,$(minimum(t.times) * 1e-9)")
+println("julia,$BLAS,matrix_multiplication,$(minimum(t.times) * 1e-9)")
 
 # Cholesky decomposition
 a = rand(Float64, (3000, 3000))
 b = copy(a)'
 c = a * b
 t = @benchmark cholesky($c)
-println("julia,cholesky,$(minimum(t.times) * 1e-9)")
+println("julia,$BLAS,cholesky,$(minimum(t.times) * 1e-9)")
 
 # SVD
 a = rand(Float64, (5000, 2000))
 t = @benchmark svd($a)
-println("julia,svd,$(minimum(t.times) * 1e-9)")
+println("julia,$BLAS,svd,$(minimum(t.times) * 1e-9)")
 
 # determinant
 a = rand(Float64, (500, 500))
 t = @benchmark det($a)
-println("julia,det,$(minimum(t.times) * 1e-9)")
+println("julia,$BLAS,det,$(minimum(t.times) * 1e-9)")
 
 # solve a linear equation system
 a = rand(Float64, (2000, 2000))
 b = 1:2000
 t = @benchmark $a \ $b
-println("julia,solve,$(minimum(t.times) * 1e-9)")
+println("julia,$BLAS,solve,$(minimum(t.times) * 1e-9)")
 
 # least squares solution
 a = rand(Float64, (2000, 1500))
 b = 1:2000
 t = @benchmark $a \ $b
-println("julia,lstsqr,$(minimum(t.times) * 1e-9)")
+println("julia,$BLAS,lstsqr,$(minimum(t.times) * 1e-9)")
--- benchmark_python.py
+++ benchmark_python.py
@@ -3,6 +3,14 @@ import random
 import numpy as np
 
 
+if hasattr(np.__config__, "openblas_info"):
+    BLAS = "openblas"
+elif np.__config__.blas_mkl_info:
+    BLAS = "mkl"
+else:
+    BLAS = "unknown"
+
+
 def _sum(numbers):
     """Compute sum of elements in x."""
     s = 0
@@ -15,48 +23,48 @@ def _sum(numbers):
 random.seed(1)
 a = [random.uniform(0, 1) for _ in range(10**7)]
 t = min(repeat(lambda: sum(a), number=50)) / 50
-print(f"python,builtin_sum,{t}")
+print(f"python,{BLAS},builtin_sum,{t}")
 
 # custom sum of 10**7 numbers
 t = min(repeat(lambda: _sum(a), number=10)) / 10
-print(f"python,custom_sum,{t}")
+print(f"python,{BLAS},custom_sum,{t}")
 
 # NumPy sum of 10**7 numbers
 a = np.random.default_rng(1).uniform(size=(10**7))
 t = min(repeat(lambda: np.sum(a), number=100)) / 100
-print(f"python,numpy_sum,{t}")
+print(f"python,{BLAS},numpy_sum,{t}")
 
 # matrix multiplication
 a = np.random.default_rng(1).uniform(size=(10000, 2000))
 b = a.copy().T
 t = min(repeat(lambda: a @ b, repeat=3, number=3)) / 3
-print(f"python,matrix_multiplication,{t}")
+print(f"python,{BLAS},matrix_multiplication,{t}")
 
 # Cholesky decomposition
 a = np.random.default_rng(1).uniform(size=(3000, 3000))
 b = a.copy().T
 c = a @ b
 t = min(repeat(lambda: np.linalg.cholesky(c), repeat=3, number=10)) / 10
-print(f"python,cholesky,{t}")
+print(f"python,{BLAS},cholesky,{t}")
 
 # SVD
 a = np.random.default_rng(1).uniform(size=(5000, 2000))
 t = min(repeat(lambda: np.linalg.svd(a), repeat=3, number=3)) / 3
-print(f"python,svd,{t}")
+print(f"python,{BLAS},svd,{t}")
 
 # determinant
 a = np.random.default_rng(1).uniform(size=(500, 500))
 t = min(repeat(lambda: np.linalg.det(a), number=1000)) / 1000
-print(f"python,det,{t}")
+print(f"python,{BLAS},det,{t}")
 
 # solve a linear equation system
 a = np.random.default_rng(1).uniform(size=(2000, 2000))
 b = np.arange(2000)
 t = min(repeat(lambda: np.linalg.solve(a, b), number=50)) / 50
-print(f"python,solve,{t}")
+print(f"python,{BLAS},solve,{t}")
 
 # least squares solution
 a = np.random.default_rng(1).uniform(size=(2000, 1500))
 b = np.arange(2000)
 t = min(repeat(lambda: np.linalg.lstsq(a, b, rcond=None), number=5)) / 5
-print(f"python,lstsq,{t}")
+print(f"python,{BLAS},lstsq,{t}")
